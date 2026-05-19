92dda629f1ec819f46ffe2efbaaf014b492dab48
Connie Karlzen <conniekarlzen@gmail.com>
2026-05-19 11:02:25 +0200

    Initial commit

---
+++ src/test/java/iths/se/connie/authserver/AuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package iths.se.connie.authserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
