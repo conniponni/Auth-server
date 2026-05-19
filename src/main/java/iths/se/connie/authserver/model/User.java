fca5ae00ad57b4e5b219da61a09eeb188922e43a
Connie Karlzen <conniekarlzen@gmail.com>
2026-05-19 14:56:12 +0200

    directonaries + app.yml + user entity

---
+++ src/main/java/iths/se/connie/authserver/model/User.java
@@ -0,0 +1,26 @@
+package iths.se.connie.authserver.model;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+}
