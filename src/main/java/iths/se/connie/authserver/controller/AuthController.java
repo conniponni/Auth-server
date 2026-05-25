fa2aa21bb75a02bc9f72c86cbf147d1e126cd4b5
Connie Karlzen <conniekarlzen@gmail.com>
2026-05-25 13:21:49 +0200

    controller for auth

---
+++ src/main/java/iths/se/connie/authserver/controller/AuthController.java
@@ -0,0 +1,40 @@
+package iths.se.connie.authserver.controller;
+
+
+import iths.se.connie.authserver.dto.LoginRequestDTO;
+import iths.se.connie.authserver.dto.TokenResponseDTO;
+import iths.se.connie.authserver.dto.UserRegisterRequestDTO;
+import iths.se.connie.authserver.dto.UserResponseDTO;
+import iths.se.connie.authserver.service.AuthService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<UserResponseDTO> register (@Valid @RequestBody UserRegisterRequestDTO request) {
+        return ResponseEntity.ok(authService.register(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<TokenResponseDTO> login(@Valid @RequestBody LoginRequestDTO request) {
+        return ResponseEntity.ok(authService.login(request));
+    }
+
+    @GetMapping ("/jwks")
+    public ResponseEntity<Map<String, Object>> jwks() {
+        return ResponseEntity.ok(authService.publicJwkSet());
+    }
+
+}
